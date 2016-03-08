179e47f8c88eb12de47079321063a115e7fe1884
Armandh96 <armandh@iastate.edu>
2016-03-08 12:41:38 -0600

    removed extra things

---
+++ KinectUnityProject/Assets/Scripts/Food.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Food : MonoBehaviour {
+	public float lifetime=0;
+	// Use this for initialization
+	void Start () 
+	{
+		Destroy (gameObject,lifetime);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
