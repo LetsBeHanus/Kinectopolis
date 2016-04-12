6245d3a886f7d07bb72eea7c431ee25ea81ad73d
cathens <cathens@iastate.edu>
2016-04-12 12:45:17 -0500

    Adding 186 Project

---
+++ KinectUnityProject/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,41 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class Slingshot : MonoBehaviour {
+//
+//	public float speed;
+//
+//	private Rigidbody rb;
+//
+//	public Rigidbody2D projectile;		
+//
+//	private SpringJoint2D spring;			
+//
+//	// Use this for initialization
+//	void Start () {
+//		
+//		//rb = GetComponent<Rigidbody2D> ();
+//
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+////		if(){
+////			
+////		}
+//
+//	}
+//
+//	void moveProjectile () {
+//
+//		float moveHorizontal = Input.GetAxis ("Horizontal");
+//		float moveVertical = Input.GetAxis ("Vertical");
+//
+//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+//
+//		rb.AddForce (movement * speed);
+//
+//	}
+		
+//}
+	
