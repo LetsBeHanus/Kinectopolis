bb356b4abd6d54e39df0bbaf51b99bb4da3e1dca
Armandh96 <armandh@iastate.edu>
2016-04-12 12:16:55 -0500

    changes to food ninja

--- KinectUnityProject/Assets/Scripts/DestroyFood.cs
+++ KinectUnityProject/Assets/Scripts/DestroyFood.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class DestroyFood : MonoBehaviour 
 {
+	//private Text scoreReference;
 	public GameObject foodReference;
-	void OnCollisionEnter(Collision col)
+	private Scorer scorer;
+	private Text scoreReference;
+	private int points=1;
+	private int score;
+
+
+	void Start()
+	{
+		score = 1;
+		scoreReference = GameObject.Find ("Text").GetComponent<Text>();
+	}
+	void OnTriggerEnter(Collider col)
 	{
 		if(col.gameObject.name=="Line")
 		{
-			Destroy (foodReference);
+			
+			//Destroy (foodReference);
+			//scoreReference.text ="Score: "+(score+=1).ToString();
 		}
-	}
 
+
+	}
+	void OnTriggerExit(Collider col)
+	{
+		//points = 1;
+		if (col.gameObject.name == "Line") 
+		{
+			//points--;
+			//if (points > -1) 
+			//{
+			Destroy (foodReference);
+			scoreReference.text ="Score: "+score.ToString();
+
+
+			//}
+			//Destroy (foodReference);
+			//score += 1;
+
+		}
+
+
+	}
 }	
---
+++ KinectUnityProject/Assets/Scripts/Scorer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Scorer : MonoBehaviour 
+{
+	public GameObject food;
+	private int score;
+	private Text textBox;
+	void Start () 
+	{
+		//score = 0;
+		//UpdateScore ();
+		//textBox = GetComponent<Text> ();
+
+	}
+	
+	//public void AddScore(int newScore)
+	//{
+	//	score += newScore;
+	//	UpdateScore ();
+	//}
+	//void UpdateScore () 
+	//{
+		
+		//textBox.text = "Score" + score;
+
+	//}
+}
